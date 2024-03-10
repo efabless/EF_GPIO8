1e95c3c5025893a584c8bcb48dd67dfb9fe93969
NouranAbdelaziz <nouran.abdelaziz@aucegpt.edu>
2024-03-10 12:52:28 +0200

    makefile and top changes

--- hdl/rtl/EF_GPIO8.pp.v
+++ hdl/rtl/EF_GPIO8.pp.v
@@ -1,26 +1,24 @@
 /*
-	Copyright 2020 AUCOHL
+	Copyright 2023 Efabless Corp.
 
-    Author: Mohamed Shalan (mshalan@aucegypt.edu)
-	
-	Licensed under the Apache License, Version 2.0 (the "License"); 
-	you may not use this file except in compliance with the License. 
-	You may obtain a copy of the License at:
+	Author: Mohamed Shalan (mshalan@aucegypt.edu)
 
-	http://www.apache.org/licenses/LICENSE-2.0
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
 
-	Unless required by applicable law or agreed to in writing, software 
-	distributed under the License is distributed on an "AS IS" BASIS, 
-	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-	See the License for the specific language governing permissions and 
+	    http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
 	limitations under the License.
+
 */
 
 `timescale			    1ns/1ps
 `default_nettype		none
-
-`define     PED(clk, sig, pulse)    reg last_``sig``; always @(posedge clk) last_``sig`` <= sig; assign pulse = sig & ~last_``sig``;
-`define     NED(clk, sig, pulse)    reg last_``sig``; always @(posedge clk) last_``sig`` <= sig; assign pulse = ~sig & last_``sig``;
 
 /*
     Brute-force Synchronizer
@@ -48,7 +46,7 @@ module aucohl_ped (
     input in,
     output out
 );
-    `PED(clk, in, out)
+    reg last_in; always @(posedge clk) last_in <= in; assign out = in & ~last_in;
 endmodule
 
 /*
@@ -59,7 +57,7 @@ module aucohl_ned (
     input in,
     output out
 );
-    `NED(clk, in, out)
+    reg last_in; always @(posedge clk) last_in <= in; assign out = ~in & last_in;
 endmodule
 
 /*
@@ -117,7 +115,6 @@ module aucohl_glitch_filter #(parameter N = 8, CLKDIV = 1) (
     aucohl_ticker ticker (
         .clk(clk),
         .rst_n(rst_n),
-        .en(1'b1),
         .clk_div(CLKDIV),
         .tick(tick)
     );
@@ -166,24 +163,7 @@ module aucohl_fifo #(parameter DW=8, AW=4)(
     reg [AW-1:0]  r_ptr_reg;
     reg [AW-1:0]  r_ptr_next;
     reg [AW-1:0]  r_ptr_succ;
-    // array reg 
-    wire[DW-1:0] array_reg_0 = array_reg[0];
-    wire[DW-1:0] array_reg_1 = array_reg[1];
-    wire[DW-1:0] array_reg_2 = array_reg[2];
-    wire[DW-1:0] array_reg_3 = array_reg[3];
-    wire[DW-1:0] array_reg_4 = array_reg[4];
-    wire[DW-1:0] array_reg_5 = array_reg[5];
-    wire[DW-1:0] array_reg_6 = array_reg[6];
-    wire[DW-1:0] array_reg_7 = array_reg[7];
-    wire[DW-1:0] array_reg_8 = array_reg[8];
-    wire[DW-1:0] array_reg_9 = array_reg[9];
-    wire[DW-1:0] array_reg_10 = array_reg[10];
-    wire[DW-1:0] array_reg_11 = array_reg[11];
-    wire[DW-1:0] array_reg_12 = array_reg[12];
-    wire[DW-1:0] array_reg_13 = array_reg[13];
-    wire[DW-1:0] array_reg_14 = array_reg[14];
-    wire[DW-1:0] array_reg_15 = array_reg[15];
-    
+
     // Level
     reg [AW-1:0] level_reg;
     reg [AW-1:0] level_next;      
@@ -265,4 +245,100 @@ module aucohl_fifo #(parameter DW=8, AW=4)(
     assign empty = empty_reg;
     assign level = level_reg;
   
+endmodule
+
+
+module EF_GPIO8 (
+    input  wire         clk,
+    input  wire         rst_n,
+    input  wire [7:0]   io_in,
+    output wire [7:0]   bus_in,
+    output wire [7:0]   io_out,
+    input  wire [7:0]   bus_out,
+    output wire [7:0]   io_oe,
+    input  wire [7:0]   bus_oe,
+
+    output wire pin0_hi,
+    output wire pin1_hi,
+    output wire pin2_hi,
+    output wire pin3_hi,
+    output wire pin4_hi,
+    output wire pin5_hi,
+    output wire pin6_hi,
+    output wire pin7_hi,
+    
+    output wire pin0_lo,
+    output wire pin1_lo,
+    output wire pin2_lo,
+    output wire pin3_lo,
+    output wire pin4_lo,
+    output wire pin5_lo,
+    output wire pin6_lo,
+    output wire pin7_lo,
+
+    output wire pin0_pe,
+    output wire pin1_pe,
+    output wire pin2_pe,
+    output wire pin3_pe,
+    output wire pin4_pe,
+    output wire pin5_pe,
+    output wire pin6_pe,
+    output wire pin7_pe,
+
+    output wire pin0_ne,
+    output wire pin1_ne,
+    output wire pin2_ne,
+    output wire pin3_ne,
+    output wire pin4_ne,
+    output wire pin5_ne,
+    output wire pin6_ne,
+    output wire pin7_ne
+    
+);
+    wire [7:0] sync_io_in;
+
+    aucohl_sync synchronizer[7:0] (.clk(clk), .in(io_in), .out(sync_io_in));
+    
+    assign bus_in = sync_io_in;
+    assign io_out = bus_out;
+    assign io_oe  = bus_oe;
+
+    assign pin0_hi = (sync_io_in[0] == 1'b1);
+    assign pin1_hi = (sync_io_in[1] == 1'b1);
+    assign pin2_hi = (sync_io_in[2] == 1'b1);
+    assign pin3_hi = (sync_io_in[3] == 1'b1);
+    assign pin4_hi = (sync_io_in[4] == 1'b1);
+    assign pin5_hi = (sync_io_in[5] == 1'b1);
+    assign pin6_hi = (sync_io_in[6] == 1'b1);
+    assign pin7_hi = (sync_io_in[7] == 1'b1);
+    
+    assign pin0_lo = (sync_io_in[0] == 1'b0);
+    assign pin1_lo = (sync_io_in[1] == 1'b0);
+    assign pin2_lo = (sync_io_in[2] == 1'b0);
+    assign pin3_lo = (sync_io_in[3] == 1'b0);
+    assign pin4_lo = (sync_io_in[4] == 1'b0);
+    assign pin5_lo = (sync_io_in[5] == 1'b0);
+    assign pin6_lo = (sync_io_in[6] == 1'b0);
+    assign pin7_lo = (sync_io_in[7] == 1'b0);
+
+    aucohl_ped ped_0 (.clk(clk), .in(sync_io_in[0]), .out(pin0_pe));
+    aucohl_ped ped_1 (.clk(clk), .in(sync_io_in[1]), .out(pin1_pe));
+    aucohl_ped ped_2 (.clk(clk), .in(sync_io_in[2]), .out(pin2_pe));
+    aucohl_ped ped_3 (.clk(clk), .in(sync_io_in[3]), .out(pin3_pe));
+    aucohl_ped ped_4 (.clk(clk), .in(sync_io_in[4]), .out(pin4_pe));
+    aucohl_ped ped_5 (.clk(clk), .in(sync_io_in[5]), .out(pin5_pe));
+    aucohl_ped ped_6 (.clk(clk), .in(sync_io_in[6]), .out(pin6_pe));
+    aucohl_ped ped_7 (.clk(clk), .in(sync_io_in[7]), .out(pin7_pe));
+
+    aucohl_ned ned_0 (.clk(clk), .in(sync_io_in[0]), .out(pin0_ne));
+    aucohl_ned ned_1 (.clk(clk), .in(sync_io_in[1]), .out(pin1_ne));
+    aucohl_ned ned_2 (.clk(clk), .in(sync_io_in[2]), .out(pin2_ne));
+    aucohl_ned ned_3 (.clk(clk), .in(sync_io_in[3]), .out(pin3_ne));
+    aucohl_ned ned_4 (.clk(clk), .in(sync_io_in[4]), .out(pin4_ne));
+    aucohl_ned ned_5 (.clk(clk), .in(sync_io_in[5]), .out(pin5_ne));
+    aucohl_ned ned_6 (.clk(clk), .in(sync_io_in[6]), .out(pin6_ne));
+    aucohl_ned ned_7 (.clk(clk), .in(sync_io_in[7]), .out(pin7_ne));
+
+    
+
 endmodule
--- verify/uvm-python/top.v
+++ verify/uvm-python/top.v
@@ -6,16 +6,17 @@ wire [7:0] io_out;
 wire [7:0] io_oe;
 reg CLK = 0;
 wire RESETn;
-wire [31:0] PADDR;
-wire PWRITE;
-wire PSEL;
-wire PENABLE;
-wire [31:0] PWDATA;
-wire [31:0] PRDATA;
-wire PREADY;
 wire irq;
 
-EF_GPIO8_APB uut(
+`ifdef BUS_TYPE_APB
+    wire [31:0] PADDR;
+    wire PWRITE;
+    wire PSEL;
+    wire PENABLE;
+    wire [31:0] PWDATA;
+    wire [31:0] PRDATA;
+    wire PREADY;
+    EF_GPIO8_APB uut(
     .io_in(io_in),
     .io_out(io_out),
     .io_oe(io_oe),
@@ -30,6 +31,34 @@ EF_GPIO8_APB uut(
     .PREADY(PREADY),
     .IRQ(irq)
 );
+`endif // BUS_TYPE_APB
+`ifdef BUS_TYPE_AHB
+    wire [31:0]	HADDR;
+    wire 		HWRITE;
+    wire 		HSEL = 0;
+    wire 		HREADYOUT;
+    wire [1:0]	HTRANS=0;
+    wire [31:0]	HWDATA;
+    wire [31:0]	HRDATA;
+    wire 		HREADY;
+    EF_GPIO8_AHBL uut(
+    .io_in(io_in),
+    .io_out(io_out),
+    .io_oe(io_oe),
+    .HCLK(CLK), 
+    .HRESETn(RESETn), 
+    .HADDR(HADDR), 
+    .HWRITE(HWRITE), 
+    .HSEL(HSEL), 
+    .HTRANS(HTRANS), 
+    .HWDATA(HWDATA), 
+    .HRDATA(HRDATA), 
+    .HREADY(HREADY),
+    .HREADYOUT(HREADYOUT),
+    .IRQ(irq)
+);
+`endif 
+
 
 `ifndef SKIP_WAVE_DUMP
 initial begin
